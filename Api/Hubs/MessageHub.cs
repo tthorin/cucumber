41e2ebb5ab9ea69dbc8e139ea3b9a2a0d544d73f
Marcus Kanon <el_maco@hotmail.se>
2023-01-11 10:09:14 +0100

    Added project

---
+++ Api/Hubs/MessageHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Api.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessageToUsers(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,35 @@
+using Api.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<MessageHub>("/signalr");
+
+app.Run();
