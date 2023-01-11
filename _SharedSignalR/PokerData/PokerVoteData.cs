9b478a5b21c891bef2d7a44a7c8399056c0c91f8
Thomas Thorin <thomas.thorin@gmail.com>
2023-01-11 14:50:44 +0100

    changed fib component

--- _SharedSignalR/PokerData/PokerVoteData.cs
+++ _SharedSignalR/PokerData/PokerVoteData.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿namespace SharedData;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SharedData
+
+public class PokerVoteData
 {
-    public class PokerVoteData
-    {
-        public int Value { get; set; } = "";
-    }
+    public int Value { get; set; }
 }
