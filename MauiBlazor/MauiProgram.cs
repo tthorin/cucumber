f9284333112653c50834fd4017a95497b3607c49
Marcus Kanon <el_maco@hotmail.se>
2023-01-12 15:44:18 +0100

    Push push

--- MauiBlazor/MauiProgram.cs
+++ MauiBlazor/MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using MauiBlazor.Data;
 
 namespace MauiBlazor;
 
@@ -21,8 +20,6 @@ public static class MauiProgram
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
-
-		builder.Services.AddSingleton<WeatherForecastService>();
 
 		return builder.Build();
 	}
