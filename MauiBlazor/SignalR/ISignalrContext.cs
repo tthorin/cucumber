e15e6a8f875c30e343ecad3ea129d6804cba1bec
Thomas Thorin <thomas.thorin@gmail.com>
2023-01-17 16:09:39 +0100

    started on Leader version of Poker Planning

--- MauiBlazor/SignalR/ISignalrContext.cs
+++ MauiBlazor/SignalR/ISignalrContext.cs
@@ -13,6 +13,7 @@ internal interface ISignalrContext
 	Task SendPokerQuestion(PokerObject question);
 	Task SendNavigationObject(Pages navigation);
 	Task JoinRoom();
+	public bool Connected { get; }
 	IDisposable OnPokerQuestion(Action<PokerObject> action);
 	IDisposable OnJoin(Action<JoinRoomData> action);
 	IDisposable OnPokerVote(Action<PokerVote> action);
