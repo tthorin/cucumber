0afe07f26ca1030e2488cd481a39be403f297637
Thomas Thorin <thomas.thorin@gmail.com>
2023-01-19 11:43:27 +0100

    can now select diffrent planning poker styles, client submit vote button disabled if no answer is selected

--- MauiBlazor/Factories/PokerObjectFactory.cs
+++ MauiBlazor/Factories/PokerObjectFactory.cs
@@ -2,11 +2,70 @@
 
 using _SharedSignalR.Models;
 
-internal class PokerObjectFactory
+internal static class PokerObjectFactory
 {
-	internal PokerObject GetTshirtStyle()
+	internal static PokerObject GetTshirtStyle(string roomName)
 	{
-		PokerObject tShirtStyle = new();
-		return tShirtStyle;
+		return new()
+		{
+			Room = roomName,
+			Title = "T-shirt",
+			Answers = new()
+				{
+					"XXL",
+					"XL",
+					"X",
+					"L",
+					"M",
+					"S",
+					"coffee",
+					"infinity"
+				},
+			Image = "/Images/shirt.svg"
+		};
+	}
+	internal static PokerObject GetUserStoriesStyle(string roomName)
+	{
+		return new()
+		{
+			Room = roomName,
+			Title = "User stories",
+			Answers = new()
+			{
+				"0",
+				"1",
+				"2",
+				"3",
+				"5",
+				"8",
+				"13",
+				"21",
+				"50",
+				"100",
+				"url,question-mark",
+				"coffee",
+				"infinity"
+			},
+			Image = "/Images/comments.svg"
+		};
+	}
+	internal static PokerObject GetDelegationPokerStyle(string roomName)
+	{
+		return new()
+		{
+			Room = roomName,
+			Title = "User stories",
+			Answers = new()
+			{
+				"Tell",
+				"Sell",
+				"Consult",
+				"Agree",
+				"Advise",
+				"Inquire",
+				"Delegate"
+			},
+			Image = "/Images/truck-fast.svg"
+		};
 	}
 }
