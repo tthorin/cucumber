f2d8b68bf4d1362206872820b21ddc8004a5f2b6
Thomas Thorin <thomas.thorin@gmail.com>
2023-01-23 23:08:37 +0100

    import

--- MauiBlazor/MauiProgram.cs
+++ MauiBlazor/MauiProgram.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
-using MauiBlazor.SignalR;
-using System.Reflection;
 
 namespace MauiBlazor;
 
@@ -16,15 +13,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
-		var a = Assembly.GetExecutingAssembly();
-		using var stream = a.GetManifestResourceStream("MauiBlazor.appsettings.json");
-		var config = new ConfigurationBuilder()
-			.AddJsonStream(stream)
-			.Build();
-		builder.Configuration.AddConfiguration(config);
 
 		builder.Services.AddMauiBlazorWebView();
-		builder.Services.AddSingleton<ISignalrContext>(new SignalrContext(config));
+
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
